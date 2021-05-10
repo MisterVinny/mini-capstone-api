e90c8a98c3978a8ae5918dc069f30ed7381fd149
vinnyk <ragdolleye@gmail.com>
2021-05-10 16:04:15 -0500

    Removes non-REST routes and actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,4 @@
 class ProductsController < ApplicationController
-  # Original non-REST methods
-  # def first_product
-  #   product = Product.first
-  #   render json: {product: product}
-  # end
-  
-  # def product_by_id # Handles both Query and Segment param using :id
-  #   id = params[:id]
-  #   product = Product.find_by(id: id)
-    
-  #   product == nil ? (render json: {message: "Invalid product ID"}) : (render json: {product: product})
-  # end
 
   def index
     products = Product.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  # Original non-REST routes
-  # get "/first_product", controller: "products", action: "first_product"
-  # # Query param route
-  # get "/product_by_query", controller: "products", action: "product_by_id"
-  
-  # # URL segment param route
-  # get "/product_by_seg/:id", controller: "products", action: "product_by_id"
-
   get "/products" => "products#index"
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
