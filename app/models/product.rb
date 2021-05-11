d63f73911ac05a355b45b21304ed18eb48776822
vinnyk <ragdolleye@gmail.com>
2021-05-11 15:25:53 -0500

    Adds custom model methods.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    price < 100 ? true : false
+  end
+  
+  def tax
+    (price * 0.09).round(2)
+  end
+  
+  def total
+    price + tax
+  end
+  
 end
